2ea61072514c01a6f764bf9ecbb88f75a2f885cc
Celso Borges <borgesdesantanavicentecelso@gmail.com>
2025-03-22 23:28:00 -0300

    Pequena melhora

--- cadastro_de_funcionarios/main.c
+++ cadastro_de_funcionarios/main.c
@@ -33,56 +33,56 @@ int main() {
     printf("\n-------- Cadastro de funcionarios --------\n\n\n");
 
     for (i = 0; i < 2; i++) {
-        printf("Funcionario %d:\n", i++);
+        printf("\n\nFuncionario %d:\n", i + 1);
 
         printf("Digite o nome do funcionario: ");
         fgets(cad_funcionario[i].nome.nome_f, sizeof(cad_funcionario[i].nome.nome_f), stdin);
-        cad_funcionario[i].nome.nome_f[strcspn(cad_funcionario[i].nome.nome_f, "\n")] = '\0'; // Remover o \n
+        cad_funcionario[i].nome.nome_f[strcspn(cad_funcionario[i].nome.nome_f, "\n")] = '\0';
 
         printf("Digite o sobrenome do funcionario: ");
         fgets(cad_funcionario[i].nome.sobrenome_f, sizeof(cad_funcionario[i].nome.sobrenome_f), stdin);
-        cad_funcionario[i].nome.sobrenome_f[strcspn(cad_funcionario[i].nome.sobrenome_f, "\n")] = '\0'; // Remover o \n
+        cad_funcionario[i].nome.sobrenome_f[strcspn(cad_funcionario[i].nome.sobrenome_f, "\n")] = '\0';
 
         printf("Digite a funcao do funcionario: ");
         fgets(cad_funcionario[i].funcao, sizeof(cad_funcionario[i].funcao), stdin);
-        cad_funcionario[i].funcao[strcspn(cad_funcionario[i].funcao, "\n")] = '\0'; // Remover o \n
+        cad_funcionario[i].funcao[strcspn(cad_funcionario[i].funcao, "\n")] = '\0';
 
         printf("Digite o salario do funcionario: ");
         scanf("%f", &cad_funcionario[i].salario);
-        getchar(); // Limpar o buffer de entrada após o scanf
+        getchar();
 
         printf("Digite o numero de dependentes do funcionario: ");
         scanf("%d", &cad_funcionario[i].dependentes);
-        getchar(); // Limpar o buffer de entrada após o scanf
+        getchar();
 
-        printf("Digite o endereco do funcionario:\n");
+        printf("\nDigite o endereco do funcionario:\n");
         printf("Digite o nome da rua: ");
         fgets(cad_funcionario[i].endereco.rua, sizeof(cad_funcionario[i].endereco.rua), stdin);
-        cad_funcionario[i].endereco.rua[strcspn(cad_funcionario[i].endereco.rua, "\n")] = '\0'; // Remover o \n
+        cad_funcionario[i].endereco.rua[strcspn(cad_funcionario[i].endereco.rua, "\n")] = '\0';
 
         printf("Digite o numero da rua: ");
         scanf("%d", &cad_funcionario[i].endereco.numero);
-        getchar(); // Limpar o buffer de entrada após o scanf
+        getchar();
 
         printf("Digite o complemento: ");
         fgets(cad_funcionario[i].endereco.complemento, sizeof(cad_funcionario[i].endereco.complemento), stdin);
-        cad_funcionario[i].endereco.complemento[strcspn(cad_funcionario[i].endereco.complemento, "\n")] = '\0'; // Remover o \n
+        cad_funcionario[i].endereco.complemento[strcspn(cad_funcionario[i].endereco.complemento, "\n")] = '\0';
 
         printf("Digite o CEP: ");
         scanf("%d", &cad_funcionario[i].endereco.cep);
-        getchar(); // Limpar o buffer de entrada após o scanf
+        getchar();
 
         printf("Digite o bairro: ");
         fgets(cad_funcionario[i].endereco.bairro, sizeof(cad_funcionario[i].endereco.bairro), stdin);
-        cad_funcionario[i].endereco.bairro[strcspn(cad_funcionario[i].endereco.bairro, "\n")] = '\0'; // Remover o \n
+        cad_funcionario[i].endereco.bairro[strcspn(cad_funcionario[i].endereco.bairro, "\n")] = '\0';
 
         printf("Digite a cidade: ");
         fgets(cad_funcionario[i].endereco.cidade, sizeof(cad_funcionario[i].endereco.cidade), stdin);
-        cad_funcionario[i].endereco.cidade[strcspn(cad_funcionario[i].endereco.cidade, "\n")] = '\0'; // Remover o \n
+        cad_funcionario[i].endereco.cidade[strcspn(cad_funcionario[i].endereco.cidade, "\n")] = '\0';
 
         printf("Digite o estado: ");
         fgets(cad_funcionario[i].endereco.estado, sizeof(cad_funcionario[i].endereco.estado), stdin);
-        cad_funcionario[i].endereco.estado[strcspn(cad_funcionario[i].endereco.estado, "\n")] = '\0'; // Remover o \n
+        cad_funcionario[i].endereco.estado[strcspn(cad_funcionario[i].endereco.estado, "\n")] = '\0';
     }
 
     // Exibir os dados cadastrados
